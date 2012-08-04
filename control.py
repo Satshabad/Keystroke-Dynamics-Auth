5e590d99054b1553a438574958e3134a1e99fb59
Satshabad Khalsa <satshabad.music@gmail.com>
2012-08-03 23:46:42 -0700

    user profiles working

--- control.py
+++ control.py
@@ -6,7 +6,16 @@ import userstore
 @route('/getConfidence', method='POST')
 def getConfidence():
     keyStrokeData = request.json
-    userstore.createUser(keyStrokeData['userID'], keyStrokeData['data'])
+    data = []
+    last_chr = ''
+    for event in keyStrokeData['data']:
+        if last_chr == '':
+            last_chr = chr(event['keyCode'])
+        else:
+            data.append((last_chr + chr(event['keyCode']), event['flightTime']))
+            last_chr = chr(event['keyCode'])
+
+    userstore.createUser(keyStrokeData['userID'], data)
 
 @route('/generate', method='GET')
 def servePage():
